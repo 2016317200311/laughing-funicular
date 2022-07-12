967d44c4d5a7c32fff3dc27310248fa4b398561d
ZK <1371884551@qq.com>
2022-07-12 20:08:39 +0800

    first

---
+++ Test/src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @author ：ZK
+ * @description：
+ * @version:
+ * @date
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("good");
+    }
+}
